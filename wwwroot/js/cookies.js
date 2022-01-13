2c6cb14216c406db3954b6cc0c5dfc8634ec9740
loglob <23386237+loglob@users.noreply.github.com>
2022-01-13 03:08:24 +0100

    No longer use cookies  replaced with localStorage  this prevents known problems when initiative lists get too big

--- wwwroot/js/cookies.js
+++ wwwroot/js/cookies.js
@@ -1,44 +1,22 @@
 /* cookies.js: Handles HTML cookies data and automatically stores data on tab close  */
 
 const cookie = {
-	/** The used cookie id
-	 * @type {string}
-	 * @constant
-	*/
-	cookiename: `mapdata_${map.id}`,
 	/** Loads and parses a cookie object from the document's cookies
 	 * @returns {void}
 	*/
 	load: function()
 	{
-		for(let line of document.cookie.split(";"))
+		try
 		{
-			const kvp = line.split("=", 2);
+			this.data = JSON.parse(localStorage[map.id]);
 
-			if(kvp.length != 2 || kvp[0].trim() != this.cookiename)
-				continue;
-
-			try
-			{
-				this.data = JSON.parse(unescape(atob(kvp[1])));
-				return;
-			}
-			catch
-			{
-				try
-				{
-					console.log("Falling back to URI component decoding")
-					this.data = JSON.parse(decodeURIComponent(kvp[1]));
-					return;
-				}
-				catch
-				{
-					console.error("Failed parsing cookie data:", kvp[1]);
-				}
-			}
+			if(typeof(this.data) !== "object")
+				throw null
 		}
-
-		this.data = {};
+		catch
+		{
+			this.data = {};
+		}	
 	},
 	/** Stores the cookie object.
 	 * Calls callbacks from onStoreCallbacks beforehand.
@@ -57,9 +35,7 @@ const cookie = {
 			}
 		}
 
-		// date 1 year in the future
-		const expires = new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 365);
-		document.cookie = `${this.cookiename}=${btoa(escape(JSON.stringify(this.data)))}; SameSite=strict; expires=${expires.toUTCString()};`
+		localStorage[map.id] = JSON.stringify(this.data)
 	},
 	/** Contains callbacks for finalizing cookie.data before it is stored.
 	 * @type {function[]}
